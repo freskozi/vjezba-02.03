58dd0ab38929f6990361c8b1d02ebd2e9f8231ab
freskozi <denisbandic98@gmail.com>
2023-03-02 18:11:25 +0100

    grdgdfs

---
+++ script.js
@@ -0,0 +1,8 @@
+let print="";
+for(let i=1; i<=10; i++) {
+    for(let j=1; j<=10; j++)
+    { print += (j + "*" + i + "=" + (j*i) + "\n");
+}
+print+="-----"+ "\n"
+}
+console.log("Tablica mnozenja:" + "\n" + print);
